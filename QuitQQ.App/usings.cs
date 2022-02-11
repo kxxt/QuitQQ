5dfd930d16b050dfc95b9f3b6ccc502727866025
kxxt <rsworktech@outlook.com>
2022-02-11 15:49:02 +0800

    Initialize Project.

---
+++ QuitQQ.App/usings.cs
@@ -0,0 +1,5 @@
+﻿global using System;
+
+global using QQConfig = QuitQQ.Configuration.AppConfig.QQ_Type;
+global using TelegramConfig = QuitQQ.Configuration.AppConfig.Telegram_Type;
+global using ForwardingConfig = QuitQQ.Configuration.AppConfig.Forwardings_Item_Type;
---
+++ QuitQQ.Configuration/Configuration.fs
@@ -0,0 +1,17 @@
+﻿namespace QuitQQ.Configuration
+
+open FSharp.Configuration
+
+type public AppConfig = YamlConfig<"../config.yaml", false, "", false>
+
+// Unfortunately， F#'s type abbreviation won't work in C#
+
+type public ForwardingConfig = AppConfig.Forwardings_Item_Type
+type public QQConfig = AppConfig.QQ_Type
+type public TelegramConfig = AppConfig.Telegram_Type
+
+module public ConfigManager =
+    let public readConfig() = 
+        let conf = AppConfig()
+        conf.Load("config.yaml")
+        conf
