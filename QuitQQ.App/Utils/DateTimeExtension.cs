2ae1945a489e765f6e1a9f0e7b2b827485c70987
kxxt <rsworktech@outlook.com>
2022-02-18 18:59:46 +0800

    add utility to convert unix timestamps.

---
+++ QuitQQ.App/Utils/DateTimeExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuitQQ.App.Utils
+{
+    internal static class DateTimeExtension
+    {
+        public static DateTime UnixTimeStampToBeijingDateTime(double unixTimeStamp)
+        {
+            // Unix timestamp is seconds past epoch
+            DateTime dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+            dateTime = dateTime.AddSeconds(unixTimeStamp).ToUniversalTime().AddHours(8); // Beijing Time
+            return dateTime;
+        }
+    }
+}
