7c7326c2d6b2e903db607cb66b947564ddb5dc98
kxxt <rsworktech@outlook.com>
2022-02-20 16:01:18 +0800

    feat: add an extension method for formatting exceptions.

---
+++ QuitQQ.App/Utils/ExceptionExtension.cs
@@ -0,0 +1,30 @@
+﻿using Flurl.Util;
+
+namespace QuitQQ.App.Utils;
+
+internal static class ExceptionExtension
+{
+    public static string FormatException(this Exception ex, uint indent = 0)
+    {
+        var indentStr = new String(' ', (int)indent);
+        return $@"-- BEGIN Exception Log <{ex.GetType()}> {DateTime.Now} --
+Message: {ex.Message}
+Data: 
+{ex.Data
+    .ToKeyValuePairs()
+    .Select((k, v) => $"    {k}: {v}")
+    .Aggregate(
+        (x, y) => x + '\n' + y
+    )}
+Source: {ex.Source}
+HResult: {ex.HResult}
+StackTrace: 
+{ex.StackTrace}
+InnerException: {ex.InnerException?.FormatException(indent + 4) ?? "<None>" }
+-- END Exception Log <{ex.GetType()}> {DateTime.Now} --"
+            .Split()
+            .Select(x => indentStr + x)
+            .Aggregate((x, y) => x + '\n' + y);
+    }
+}
+
