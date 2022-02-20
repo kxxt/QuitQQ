911e8b1474b3f9e66ff62b739edf446256caf43f
kxxt <rsworktech@outlook.com>
2022-02-20 16:04:00 +0800

    feat: append logs instead of overwrite them.

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
-"$SCRIPTPATH/QuitQQ.App/bin/Release/net6.0/QuitQQ.App" > QuitQQ.log 2>&1
+"$SCRIPTPATH/QuitQQ.App/bin/Release/net6.0/QuitQQ.App" >> QuitQQ.log 2>&1
