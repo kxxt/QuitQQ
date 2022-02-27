a1ec891bbfd06f8cf7775b12244fdf7b6d92e273
kxxt <rsworktech@outlook.com>
2022-02-27 09:12:53 +0800

    fix: exception formatting.

--- QuitQQ.App/Utils/ExceptionExtension.cs
+++ QuitQQ.App/Utils/ExceptionExtension.cs
@@ -6,7 +6,7 @@ internal static class ExceptionExtension
 {
     public static string FormatException(this Exception ex, uint indent = 0)
     {
-        var indentStr = new String(' ', (int)indent);
+        var indentStr = new string(' ', (int)indent);
         return $@"-- BEGIN Exception Log <{ex.GetType()}> {DateTime.Now} --
 Message: {ex.Message}
 Data: 
@@ -23,7 +23,7 @@ StackTrace:
 {ex.StackTrace}
 InnerException: {ex.InnerException?.FormatException(indent + 4) ?? "<None>" }
 -- END Exception Log <{ex.GetType()}> {DateTime.Now} --"
-            .Split()
+            .Split('\n')
             .Select(x => indentStr + x)
             .Aggregate(
                 string.Empty,
